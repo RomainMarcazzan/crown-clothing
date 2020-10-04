ae4b1b8bf94c4900098dad4d17eedf0ba37f3c5a
RomainMarcazzan <romain.marcazzan@hotmail.com>
2020-10-04 22:14:38 +0200

    update menu-items to navigate withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-// eslint-disable-next-line
+
+// eslint - disable - next - line;
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
