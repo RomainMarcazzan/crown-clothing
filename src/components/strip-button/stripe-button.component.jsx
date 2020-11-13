25a472527f198ab704446861d08a3e2240d0808f
RomainMarcazzan <romain.marcazzan@hotmail.com>
2020-11-13 22:34:45 +0100

    updating stripe integration and updating favicon + title

---
+++ src/components/strip-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "test";
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crown Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
